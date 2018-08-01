30a6966521d68b8756ed730ef4600a2c76141125
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-01 13:59:21 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -19,4 +19,32 @@ client.on('ready', () => {
   console.log('╚[════════════════════════════════════]╝')
 });
 
+
+
+client.on('message', message => {
+	const prefix = 'T'
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » Message : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
