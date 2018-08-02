93b59ab2ba449bde64fa85237f93d249d5a9639b
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-01 17:29:28 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -146,14 +146,18 @@ client.on('message', message => {
   } else 
   if (message.content.startsWith(adminprefix + 'streaming')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`** ${client.user} STREAMING  :space_invader:  ** **${argresult}** `)
-     let embed = new Discord.RichEmbed()
+        if(!message.channel.guild) return;
+                            var msg = `${Date.now() - message.createdTimestamp}`
+                            var api = `${Math.round(client.ping)}`
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
                         .setAuthor(message.author.username,message.author.avatarURL)
                         .setColor('RANDOM')
-                        .addField(' ${client.user} STREAMING  :space_invader:  ${argresult}")
+                        .addField('**PING :**',api + " ms ⚡ ")
          message.channel.send({embed:embed});
                         }
-  
+                    });
+
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.send(`**Changing The Name To , :zap: **${argresult}** `)
