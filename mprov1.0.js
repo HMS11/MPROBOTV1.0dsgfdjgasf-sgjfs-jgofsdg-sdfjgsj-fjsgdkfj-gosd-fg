9c2e714bffd3115cd207da5435a4645efb5cfb5b
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-02 12:52:16 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -388,88 +388,50 @@ const moment = require('moment');
 
 
 
-  client.on("ready", () => {
 
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+}
+client.on("ready", () => {
     var guild;
-
     while (!guild)
-
         guild = client.guilds.get("474312438804906004");
-
     guild.fetchInvites().then((data) => {
-
         data.forEach((Invite, key, map) => {
-
             var Inv = Invite.code;
-
             dat[Inv] = Invite.uses;
-
         });
-
     });
-
 });
 
- 
 
- 
-
- 
 
 client.on("guildMemberAdd", (member) => {
-
     let channel = member.guild.channels.get("474484145977032704");
-
     if (!channel) {
-
         console.log("!the channel id it's not correct");
-
         return;
-
     }
-
     if (member.id == client.user.id) {
-
         return;
-
     }
-
     console.log('-');
-
     var guild;
-
     while (!guild)
-
         guild = client.guilds.get("474312438804906004");
-
     guild.fetchInvites().then((data) => {
-
         data.forEach((Invite, key, map) => {
-
             var Inv = Invite.code;
-
             if (dat[Inv])
-
                 if (dat[Inv] < Invite.uses) {
-
- channel.send(`Welcome To Cilęo Server.
-Invited By :   ${Invite.inviter} `) ;       
-
+ channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
  }
-
             dat[Inv] = Invite.uses;
-
        
-
        });
-
     });
-
 });
-  
-
-
-
 
 
 client.login(process.env.BOT_TOKEN);
