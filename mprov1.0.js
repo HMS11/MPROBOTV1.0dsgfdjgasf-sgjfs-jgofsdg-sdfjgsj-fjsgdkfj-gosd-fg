431ff04c53a60f0893b76e43b0dbfcaff1849305
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-02 00:50:53 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -295,4 +295,47 @@ if( verifed.some(word => message.author.id.includes(word)) ) {    return message
 }
 });
 
+
+
+
+
+
+
+
+client.on('message', message => {
+const prefix = "#";
+  if (message.author.kick) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "kick") {
+               if(!message.channel.guild) return;
+         
+  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
+  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+
+  if (message.mentions.users.size < 1) return message.reply("اكتب الاسم");
+  if(!reason) return message.reply ("اكتب سبب الطرد");
+  if (!message.guild.member(user)
+  .bannable) return message.reply(" لا يمكنك اعطاء كيك للأدارة");
+
+  message.guild.member(user).kick(7, user);
+
+  const banembed = new Discord.RichEmbed()
+  .setAuthor('Kicked !', user.displayAvatarURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .addField("User:",  `[ + ${user.tag} + ]`)
+  .addField("By:", `[  + ${message.author.tag} +  ]`)
+  .addField("Reason:", `[ + ${reason} +  ]`)
+  client.channels.get("474484145977032704").send({embed : banembed})
+}
+});
+
 client.login(process.env.BOT_TOKEN);
