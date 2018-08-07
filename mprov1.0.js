92f1e615b1bb17fdb0eb2bf4895fca583c83ae23
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-07 07:29:07 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -1410,75 +1410,22 @@ client.on('message', message => {
 })
 
 
-
-
-
-client.on('message', emoko => {
-  if (emoko.content === "+date") {
-         if (!emoko.channel.guild) return emoko.reply('** This command only for servers **');  
-         var currentTime = new Date(),
-            hours = currentTime.getHours() + 4 ,
-            hours2 = currentTime.getHours() + 3 ,
-            hours3 = currentTime.getHours() + 2 ,
-            hours4 = currentTime.getHours() + 3 ,
-            minutes = currentTime.getMinutes(),
-            seconds = currentTime.getSeconds(),
-            Year = currentTime.getFullYear(),
-            Month = currentTime.getMonth() + 1,
-            Day = currentTime.getDate();
-             var h = hours
-  if(hours > 12) {
-               hours -= 12;
-            } else if(hours == 0) {
-                hours = "12";
-            }  
-             if(hours2 > 12) {
-               hours2 -= 12;
-            } else if(hours2 == 0) {
-                hours2 = "12";
-            
-            }  
-if(hours3 > 12) {
-               hours3 -= 12;
-            } else if(hours3 == 0) {
-                hours3 = "12";
-            } 
-            if (minutes < 10) {
-                minutes = '0' + minutes;
-            }
-
-
-            var suffix = 'صباحاَ';
-            if (hours >= 12) {
-                suffix = 'مساء';
-                hours = hours - 12;
-            }
-            if (hours == 0) {
-                hours = 12;
-            }
- 
-
-const embed = new Discord.RichEmbed()
-                .setThumbnail("https://cdn.discordapp.com/avatars/474311458323300362/46f5b63fd4b58630d3c57a5b0fadadf1.png") 
-                .setTitle( "『التاريخ  والوقت』")
-                .setColor('RANDOM')
-                .setFooter(emoko.author.username, emoko.author.avatarURL)
-                .addField('KSA',
-                "『"+ hours + ":" + minutes +":"+ seconds + "』")
-                 .addField('SA',
-                "『"+ hours2 + ":" + minutes +":"+ seconds  + "』") 
-                .addField('EGYPT',
-                "『"+ hours3 + ":" + minutes +":"+ seconds  + "』") 
-                
-                .addField('Date',
-                "『"+ Day + "-" + Month + "-" + Year +  "』")
-
-
-  emoko.channel.sendEmbed(embed);
-   }
-});
-
-
+  client.on('message', message => {
+  
+    if(message.content.split(' ')[0] == '#talkwithstaff'){
+         if(!message.channel.guild) return;
+                            let args = message.content.split(' ').slice(1).join(' ');
+    
+    client.guilds.get("474312438804906004").members.get("459300517999411218").sendMessage(message.author.tag+"\n Message : "+args)
+    
+                                                    let embed = new Discord.RichEmbed()
+                                                    .setAuthor(message.author.username, message.author.avatarURL)
+                                                    .setDescription('📬 تم ارسال الرسالة  بنجاح')
+                                                    .setThumbnail(message.author.avatarURL)
+                                                    .setFooter(message.author.username, message.author.avatarURL)
+                                                    message.channel.sendEmbed(embed);}
+                                                  });
+  
 
 
 
