04ecdf2dcb5676f0f4a39793b0e6f8a260d10148
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-10 02:17:44 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -1972,7 +1972,7 @@ client.on("channelCreate",  cc => {
   }
   });
 
-   client.on("deleteChannel",  dc => {
+   client.on("channelDelete",  dc => {
   const channel = dc.guild.channels.find("name", "log")
   if(channel) {
   var embed = new Discord.RichEmbed()
@@ -1988,7 +1988,29 @@ client.on("channelCreate",  cc => {
 
 
 
+client.on('voiceStateUpdate', (o, n) => {
+        let newUserChannel = n.voiceChannel
+    let oldUserChannel = o.voiceChannel
 
+    var channel = client.channels.get("475242008571674625");
+        let cha = n.guild.channels.get("477311257561530368");
+        
+          let mute1 = o.serverMute;
+  let mute2 = n.serverMute;
+  
+
+  let deafen1 = o.serverDeaf;
+  let deafen2 = n.serverDeaf;
+
+    if(mute1 === false && mute2 === true) return;
+    if(mute1 === true && mute2 === false) return;
+    if(deafen1 === false && deafen2 === true) return;
+    if(deafen1 === true && deafen2 === false) return;
+    
+
+    channel.send(`Join Room ${n.displayName}`)
+
+})
 
 
 
