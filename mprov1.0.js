3eecb1eba645e8e12582f0124cd56bcfa10c9d06
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-11 14:02:25 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -2131,4 +2131,45 @@ Your Level : ${voice[message.member.id].level}`);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const fs = require('fs');
+const db = fs.database();
+client.oN("message", message => {
+        if (message.content === "setprefix") {
+        if (message.author.id !== "459300517999411218" && !message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`U don't have permission to do that`);
+  let args = message.content.split(" ").slice(1);
+        let arg = args.join("").substring(message.length)
+        if (!arg) return message.channel.send(`Please add a prefix after command like \`\`${prefix}setprefix &\`\``);
+        fs.database().ref('servers/' + message.guild.id).update({
+            guildname: message.guild.name,
+            guildprefix: arg
+        }).catch(function(err) {
+            message.channel.send(err + "\n\n\n");
+        });
+        message.channel.send(`prefix updated ${arg} for ${message.guild.name}`);
+    }
+});
+  
+
+
+
+
 client.login(process.env.BOT_TOKEN);
