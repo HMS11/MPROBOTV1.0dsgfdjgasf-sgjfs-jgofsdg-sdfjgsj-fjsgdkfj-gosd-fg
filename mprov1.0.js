60ae7eeee0a343563a8933aab73c23beec36cf17
ichbinxirdx <40479432+ichbinxirdx@users.noreply.github.com>
2018-08-11 13:57:26 -0700

    Update mprov1.0.js

--- mprov1.0.js
+++ mprov1.0.js
@@ -2074,28 +2074,60 @@ mentionned.send(` :credit_card: | Transfer Receipt \`\`\`You have received ${arg
 
 
 
+  var returned;
+client.on('voiceStateUpdate', (user, member) => {
+  if(member.selfDeaf || member.selfMute || member.serverDeaf || member.serverMute) {
+    returned = false;
+  }
+  if(!member.selfDeaf || !member.selfMute ||!member.serverDeaf || !member.serverMute) {
+    returned = true;
+  }
+  setInterval(() => {
+    if(returned === true) {
+      if(member.bot) return;
+      if(!member.voiceChannel) returned = false;
+      if(!voice[member.id]) voice[member.id] = {
+        xp: 1,
+        level: 1
+      };
+      voice[member.id] = {
+        xp: voice[member.id].xp + Math.floor(Math.random() * 4) + 1,
+        level: voice[member.id].level
+      };
+      var curXp = voice[member.id].xp;
+      var curLvl = voice[member.id].level;
+      if(curXp >= 300) {
+        voice[member.id] = {
+          xp: 1,
+          level: curLvl + 1
+        };
+      }
+      fs.writeFile('./voiceState.json', JSON.stringify(voice, null, 4), (e) => {
+        if(e) console.log(e);
+      });
+    } else if(returned === false) {
+      return null;
+    }
+  },5000);
+});
+  
+
+
+const voice = JSON.parse(fs.readFileSync("./voiceState.json", "utf8"));
 
 
 
 
-
-
-
-
-
-  client.on('message', async message => {
+client.on('message', async message => {
 if(message.author.bot) return;
 if (message.channel.guild) {
-if (message.content.startsWith(prefix + `search`)) {
-            const textQuery = message.content.split(' ').slice(1).join(' ');
-        const rebel = encode(message.content.split(' ').slice(1).join(' '));
-        const url = `https://lmgtfy.com/?q=${rebel}`;
-
-        if (!rebel) return message.channel.send(`من فضلك أكتب البحث , مثآل : \`${prefix}Lmgtfy How to create a Discord server\`.`);
-        else message.channel.send(`"${textQuery}"\n**<${url}>**`);
+if (message.content === '#voicestate') {
+message.channel.send(`Your XP : ${voice[message.member.id].xp}
+Your Level : ${voice[message.member.id].level}`);
+      fs.writeFile('./voiceState.json', JSON.stringify(voice, null, 4), (e) => {
+        if(e) console.log(e);
+      });
 }}});
-
-
 
 
 
